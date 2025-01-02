f1893f7de21da793cc54a0e1a14e67793c0eb0ff
Me <fomidiya@gmail.com>
2025-01-01 17:37:38 -0700

    Old frontend

--- frontend/src/Pages/Polls.js
+++ frontend/src/Pages/Polls.js
@@ -1,9 +1,11 @@
 import React from "react";
+import BarGraph from "../components/BarGraph";
 
 function Polls() {
   return (
     <div>
       <h1>Polls</h1>
+      <BarGraph stateName="Alaska" />
     </div>
   );
 }
